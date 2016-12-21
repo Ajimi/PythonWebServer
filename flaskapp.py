fa364fdeb73396c71b42c348c87b97ca1c325be7
ajimi <sloumma.21@gmail.com>
2016-12-21 14:31:27 +0100

    adding routing functionality

--- flaskapp.py
+++ flaskapp.py
@@ -12,9 +12,9 @@ session = DBSession()
 
 
 @app.route('/')
-@app.route('/hello')
-def HelloWorld():
-	restaurant = session.query(Restaurant).first() #Getting the first restaurant using sqlalchemy
+@app.route('/restaurants/<int:restaurant_id>/')
+def restaurantMenu(restaurant_id):
+	restaurant = session.query(Restaurant).filter_by(id = restaurant_id).one() # get the restaurant id using the route
 	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id) #Getting the item list by id of restaurant
 	output = ""
 	for item in items :
@@ -26,6 +26,18 @@ def HelloWorld():
 		output += "<br>" 
 		output += "<br>" 
 	return output
+
+@app.route('/restaurant/<int:restaurant_id>/new')
+def newMenuItem(restaurant_id):
+    return "page to create a new menu item. Task 1 complete!"
+
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/edit')
+def editMenuItem(restaurant_id, menu_id):
+    return "page to edit a menu item. Task 2 complete!"
+    
+@app.route('/restaurant/<int:restaurant_id>/<int:menu_id>/delete')
+def deleteMenuItem(restaurant_id, menu_id):
+    return "page to delete a menu item. Task 3 complete!"
 
 if __name__ == '__main__':
     app.debug = True #allow flask server to instantly refresh it's own self 
