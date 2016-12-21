50bff3dca13652a295af5cee4186ea52ff4af146
ajimi <sloumma.21@gmail.com>
2016-12-21 13:35:48 +0100

    showing the list of restaurant

--- flaskapp.py
+++ flaskapp.py
@@ -1,11 +1,28 @@
 from flask import Flask
 app = Flask(__name__)
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from database_setup import Base, Restaurant, MenuItem
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.bind = engine
+
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
 
 
 @app.route('/')
 @app.route('/hello')
 def HelloWorld():
-    return "Hello World"
+	restaurant = session.query(Restaurant).first()
+	items = session.query(MenuItem).filter_by(restaurant_id = restaurant.id)
+	output = ""
+	for item in items :
+		output += item.name
+		output += "<br>"
+
+	print output
+	return output
 
 if __name__ == '__main__':
     app.debug = True
